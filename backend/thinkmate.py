14ea2f755560088cb82c2076822977387cd1970e
MohammedPathariya <pathmohd123@gmail.com>
2025-06-17 11:05:24 -0400

    Add Docker Support

--- backend/thinkmate.py
+++ backend/thinkmate.py
@@ -555,4 +555,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     print("🧠 Flask app starting. Database:", db_path)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
